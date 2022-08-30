5a3a1349ae1a76377940e840039cfdc3f563c42d
Stephen G. Novosel, J.D <sgnovo8@gmail.com>
2022-08-30 18:05:01 -0400

    corrected added comment marks

--- FundMe.sol
+++ FundMe.sol
@@ -14,7 +14,7 @@ contract FundMe {
 //  eth is great but maybe we want to work in USD or some other token, so
 //  how would we do that; if we know what the min we want in Eth is, how 
 //  do we convert from whatever they want to give us to what we can use in this SK?
-o if you know what you want to convert, say Eth -> USD, you have to know 
+//  if you know what you want to convert, say Eth -> USD, you have to know 
 //  what the conversion rate is
 //  how do you know that?  You have to use an external source to generate it for you
 //  see "The Oracle Problem";  SKs can't connect with external systems, data feeds, APIs, or any 
